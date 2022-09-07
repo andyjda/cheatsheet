3da222dab4e5e0c24793ba1269bb88414c3428ed
andyjda <bobodeangelis@gmail.com>
2022-09-07 15:44:33 -0400

    abiltiy to delete list and read list from separate file

--- cheatsheet.el
+++ cheatsheet.el
@@ -1,8 +1,9 @@
 ;;; cheatsheet.el --- create your own cheatsheet
 ;; I (Andrew De Angelis) am working on this package for possible enhancements
 ;;    bobodeangelis@gmail.com
-
-;; Copyright (C) 2015 Shirin Nikita and contributors
+;;
+;; Package-Requires: ((emacs "28") (cl-lib "0.5"))
+;; Version: 1.0
 ;; Keywords: convenience, usability
 
 ;; This file is not part of GNU Emacs
@@ -25,13 +26,19 @@
 
 (require 'cl-lib)
 
+(defgroup cheatsheet nil
+  "Display a cheatsheet of emacs command."
+  :group 'convenience)
+
 (defface cheatsheet-group-face
   '((t :foreground "red"))
-  "Group name font face.")
+  "Group name font face."
+  :group 'cheatsheet)
 
 (defface cheatsheet-key-face
   '((t :foreground "orange"))
-  "Cheat key font face.")
+  "Cheat key font face."
+  :group 'cheatsheet)
 
 
 (defvar cheatsheet--cheat-list '()
@@ -121,20 +128,46 @@
   "/Users/andrewdeangelis/.emacs.d/elpa/cheatsheet-20170126.2150/cheatsheet.el"
   "Variable for the path to the file where the cheatsheet list is kept")
 
-(defun cheatsheet--save-list-on-exit ()
-  "When closing emacs, save the current cheatsheet list.
+(defvar set-list-rgx "^(setq cheatsheet--cheat-list '(+.*)+"
+  "regex used to find where the value of our cheat list is set")
+
+;; can be used to ensure the list is saved when exiting emacs,
+;; by adding:
+;; (add-hook 'kill-emacs-hook #'cheatsheet--save-list-to-elisp-file)
+;; to the init file
+;;;###autoload
+(defun cheatsheet-save-list-to-elisp-file ()
+  "Save the current cheatsheet list.
 Check if the list has been modified, and if so,
 re-write the 'setq' call in this file"
   (if cheatsheet--modifiedp
       (with-current-buffer (find-file-noselect cheatsheet--path-to-list-file)
-	(replace-regexp "(setq cheatsheet--cheat-list '(+.*)+"
-			(format "(setq cheatsheet--cheat-list '%s)"
-				cheatsheet--cheat-list)))))
+	;; (replace-regexp set-list-rgx
+	;; 		(format "(setq cheatsheet--cheat-list '%s)"
+	;; 			cheatsheet--cheat-list)))))
+	(re-search-forward set-list-rgx)
+	(replace-match (format "(setq cheatsheet--cheat-list '%s)"
+			       (if cheatsheet--cheat-list
+				   (prin1-to-string cheatsheet--cheat-list)
+				 "()")))
+	(save-buffer)
+	(setq cheatsheet--modifiedp nil))))
+
+(defun cheatsheet-clear-list ()
+  (interactive)
+  (setq cheatsheet--cheat-list '())
+  (setq cheatsheet--modifiedp t))
 
 ;; this variable is custom-set by the above function.
 ;; It should not be broken up with newlines
 ;; (unless you want to change the above regexp accordingly)
-(setq cheatsheet--cheat-list '((:group Common :key "C-x C-c" :name "save-buffers-kill-terminal" :description "leave Emacs.")))
+(setq cheatsheet--cheat-list '((:group Common :key "C-x C-c" :description "leave Emacs.")))
+
+;; it will probably be better to instead store the list in its own file
+(defun get-lisp-object-from-file (filePath)
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (read (current-buffer))))
 
 ;; Interface
 ;;;###autoload
